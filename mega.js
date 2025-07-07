c25145d57f008b8513cee2bfc50246b8e69a0666
sanijamdtester <sanijamd70@gmail.com>
2025-07-07 17:30:01 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -5,7 +5,7 @@ import * as mega from 'megajs';
 // Mega authentication credentials
 const auth = {
     email: 'sanijamd70@gmail.com', // Replace with your Mega email
-    password: '0767858145sanijamd', // Replace with your Mega password
+    password: '0767858145sanijamd2', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
